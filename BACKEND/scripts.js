77d0d79d5294cfe15214d72e8ed8c6d2f41960a3
Danilsa Helena <danilsawk@gmail.com>
2024-10-19 17:48:35 +0100

    Update scripts.js

--- BACKEND/scripts.js
+++ BACKEND/scripts.js
@@ -11,3 +11,46 @@ $(document).ready(function () {
     // Chama a função de movimentação a cada 2 segundos
     setInterval(moveImages, 2000);
 });
+
+
+
+
+
+// Mensagens de tela animadas
+window.addEventListener('DOMContentLoaded', (event) => {
+    const promoText = document.querySelector('.promo-text');
+    promoText.classList.add('animate__animated', 'animate__fadeInDown');
+
+    // Mensagem de contato rápida
+    setTimeout(() => {
+        alert('Aproveite a promoção de pacotes estratégicos por 5 mil Kz!');
+    }, 5000);
+});
+
+
+// Array de imagens de fundo para o carrossel
+const banners = [
+    'url("IIMG/banner1.jpg")',
+    'url("IIMG/banner2.jpg")',
+    'url("IIMG/banner3.jpg")'
+];
+
+// Seleciona o header
+const header = document.getElementById('carousel-header');
+let currentBanner = 0;
+
+// Função para trocar o fundo do header
+function changeBanner() {
+    currentBanner = (currentBanner + 1) % banners.length; // Volta ao início após o último banner
+    header.style.backgroundImage = banners[currentBanner];
+}
+
+// Troca de banner a cada 5 segundos (5000 ms)
+setInterval(changeBanner, 5000);
+
+// Define o primeiro banner ao carregar a página
+window.onload = () => {
+    header.style.backgroundImage = banners[currentBanner];
+};
+
+
